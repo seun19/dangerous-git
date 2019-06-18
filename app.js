882935765bbc3630a7b90ee7c21ec3422054b394
Seun Olaifa <seunolaifa19@yahoo.com>
2019-06-18 15:57:09 +0100

    Initialize Repo

---
+++ app.js
@@ -0,0 +1,9 @@
+
+const call = () =>{
+    fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
+        .then(response => response.json())
+        .then(commits => alert(commits[0].author.login));
+}
+
+call();
+
